d5239678ea35630bb6803257ad00d756f9d3222e
Talin Daga <talindaga692@gmail.com>
2026-02-22 01:12:03 +0530

    Test frontend and backend fix

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from django.contrib.auth import authenticate, login, logout
 
 
 @csrf_exempt
